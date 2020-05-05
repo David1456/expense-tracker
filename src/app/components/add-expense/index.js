cd2ddfcb78f30efbd21fa4859219750ff18df371
balaji1-456 <balaji.kka@gmail.com>
2020-05-05 15:54:44 +0530

    storing expenses in localStorage

--- src/app/components/add-expense/index.js
+++ src/app/components/add-expense/index.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Title, StyledInput, Button } from '../../styled-components';
+
+const AddExpense = ({ type }) => {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+
+    const handleAddExpense = () => {
+        const newTransaction = {
+            title, amount, date: getCurrentTime(), type
+        }
+
+        if (localStorage.getItem("transactions")) {
+            let transactions = JSON.parse(localStorage.getItem("transactions"))
+            transactions = [...transactions, newTransaction]
+            localStorage.setItem("transactions", JSON.stringify(transactions))
+        }
+        else localStorage.setItem("transactions", JSON.stringify([newTransaction]))
+    }
+
+    return (
+        <div style={containerStyle}>
+            <Title>Add New Expense</Title>
+            <div>
+                <Title>Add Title</Title>
+                <StyledInput onChange={(e) => setTitle(e.target.value)} value={title} />
+            </div>
+            <div>
+                <Title>Add Amount</Title>
+                <StyledInput type="number" min="0" onChange={(e) => setAmount(e.target.value)} value={amount} />
+            </div>
+            <Button primary onClick={handleAddExpense}>{`Add ${
+                type === 'income' ? 'Income' : 'Spending'
+                }`}</Button>
+        </div>
+    );
+}
 
 const containerStyle = {
     display: 'flex',
@@ -8,22 +43,14 @@ const containerStyle = {
     alignItems: 'center'
 }
 
-const AddExpense = ({ type }) => {
+const getCurrentTime = () => {
+    const instance = new Date()
+    const date = instance.getDate()
+    const month = instance.getMonth()
+    const year = instance.getFullYear()
 
-    return <div style={containerStyle}>
-        <Title>Add New Expense</Title>
-        <div>
-            <Title>Add Text</Title>
-            <StyledInput />
-        </div>
-        <div>
-            <Title>Add Amount</Title>
-            <StyledInput type="number" min="0" />
-        </div>
-        <Button primary>{`Add ${
-            type === 'income' ? 'Income' : 'Spending'
-            }`}</Button>
-    </div>
+    return {
+        date, month, year
+    }
 }
-
 export default AddExpense;
