0be221896c6c5a11497f456d10fce4b03156748a
balaji1-456 <balaji.kka@gmail.com>
2020-05-05 13:56:02 +0530

    added pages routes

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Home from './pages/home';
+import Add from './pages/add';
 
 const App = () => {
-    return <div className='app-container'>
-        App
-    </div>
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/add" component={Add} />
+                <Route path="/" component={Home} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
